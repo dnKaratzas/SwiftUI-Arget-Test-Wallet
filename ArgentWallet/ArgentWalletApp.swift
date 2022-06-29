a4502276c17b70e9e00e7144f17d0c34c8607da6
Dionysios Karatzas <dkaratzas@hotmail.com>
2022-06-29 16:16:37 +0300

    Initial Commit

---
+++ ArgentWallet/ArgentWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgentWalletApp.swift
+//  ArgentWallet
+//
+//  Created by Dionisis Karatzas on 29/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ArgentWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArgentWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArgentWallet
+//
+//  Created by Dionisis Karatzas on 29/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
